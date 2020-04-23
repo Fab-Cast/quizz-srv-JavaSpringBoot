887fefe6185912f0be9bb7e3bea5583ea08345c8
Fab Cast <f.castagnet@gmail.com>
2020-04-23 13:56:01 +1100

    Authentication JWT

--- src/main/java/com/checkskills/qcm/controller/QcmController.java
+++ src/main/java/com/checkskills/qcm/controller/QcmController.java
@@ -7,6 +7,7 @@ import com.checkskills.qcm.repository.QcmRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -19,27 +20,32 @@ public class QcmController {
     private QcmRepository qcmRepository;
 
     @GetMapping("/qcm")
+    @PreAuthorize("hasRole('EMPLOYER')")
     public List<QcmLite> getAllQcmLite(){
         return qcmRepository.findAllQcmLite();
     }
 
     @GetMapping("/qcm/{id}")
+    @PreAuthorize("hasRole('EMPLOYER') or hasRole('AUTHOR')")
     public Optional<Qcm> getQcmById(@PathVariable(value = "id") Long id) {
         return qcmRepository.findById(id);
     }
 
     @PostMapping("/qcm")
+    @PreAuthorize("hasRole('AUTHOR')")
     public Qcm createQcm(@RequestBody Qcm qcm){
         return qcmRepository.save(qcm);
     }
 
     @PutMapping("/qcm/{id}")
+    @PreAuthorize("hasRole('AUTHOR')")
     public Qcm updateQcm(@RequestBody Qcm qcm, @PathVariable Long id){
         qcm.setId(id);
         return qcmRepository.save(qcm);
     }
 
     @DeleteMapping(value = "/qcm/{id}")
+    @PreAuthorize("hasRole('AUTHOR')")
     public ResponseEntity<Void> deleteQcm(@PathVariable Long id) {
         try {
             qcmRepository.deleteById(id);
